c1ea09c30d668d50a00f38659032c807e5723e8d
Brandon Ruiz <Sober4202020@outlook.com>
2023-02-12 07:38:05 +0000

    added last question with whileloop

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 'user strict';
-
 let responce = prompt('Welcome to my website, what is your name?..');
 alert('Hello and welcome to my website ' + responce);
 let responceTwo = prompt('Do I have Brown hair? Answer Yes or No ');
@@ -16,9 +15,9 @@ let responceThree = prompt('Is today Monday? Answer yes or no');
 let userDatatwo = responceThree.toLowerCase();
 if(userDatatwo === null){
   alert('Please answer!!');
-} else if (userDatatwo === 'yes' || userDatatwo === 'y');{
+} else if (userDatatwo === 'yes' || userDatatwo === 'y'){
   alert('Correct');
-} else if (userDatatwo === 'no' || userDatatwo == 'n'{
+} else if (userDatatwo === 'no' || userDatatwo === 'n'){
   alert('Incorrect');
 }
 
@@ -28,7 +27,7 @@ if(userDatathree === null){
   alert('Please answer!!');
 } else if (userDatathree === 'yes' || userDatathree === 'y') {
   alert('Correct');
-} else if (userDatathree === 'no' || userDatathree == 'n') {
+} else if (userDatathree === 'no' || userDatathree === 'n') {
   alert('Incorrect');
 }
 
@@ -52,7 +51,24 @@ if(userDatafive === null){
   alert('Incorrect');
 }
 
-alert("Thank you for visiting my site " + responce);
 
+
+
+let r = 6;
+
+while (r) {
+  let responceSeven = prompt('Guess a number enter numeric value here ');
+  if(responceSeven === null){
+    alert('Please answer!!');
+
+  // eslint-disable-next-line eqeqeq
+  } else if (responceSeven == 6) {
+    alert('Correct');
+    r = false;
+  } else if (responceSeven < r) {
+    alert('Incorrect');
+
+  }
+}
 
 
